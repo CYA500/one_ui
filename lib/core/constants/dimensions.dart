d3431b8b0ec2eb97630a311c4fd30ecd1d2ba7e8
yakin <billfearry@gmail.com>
2026-05-21 07:52:35 +0100

    Add AppDimensions class with constant values

---
+++ lib/core/constants/dimensions.dart
@@ -0,0 +1,10 @@
+// lib/core/constants/dimensions.dart
+class AppDimensions {
+  AppDimensions._();
+  static const double lockScreenPadding = 24.0;
+  static const double nowBarHeight = 56.0;
+  static const double nowBarWidthRatio = 0.85;
+  static const double shortcutSize = 48.0;
+  static const double notificationWidth = 320.0;
+  static const double clockToTopRatio = 0.25;
+}
