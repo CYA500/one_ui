ae053a6e7ecd4a63cc6de006c1cbcd3fb6d0d3df
yakin <billfearry@gmail.com>
2026-05-21 08:23:21 +0100

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/oneui85/simulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oneui85.simulator
+
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
+}
